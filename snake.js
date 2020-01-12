34a9a8fcbb2297e481065dd8f7623b96809d2a33
Luka-mac1 <55154321+Luka-mac1@users.noreply.github.com>
2020-01-11 19:01:37 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -2,7 +2,9 @@
 // http://codingtra.in
 // http://patreon.com/codingtrain
 // Code for: https://youtu.be/AaGK-fj-BAM
-
+var h=0;
+var j = 0;
+var count = 0 ;
 function Snake() {
   this.x = 0;
   this.y = 0;
@@ -10,20 +12,38 @@ function Snake() {
   this.yspeed = 0;
   this.total = 0;
   this.tail = [];
-
   this.eat = function(pos) {
     let d = dist(this.x, this.y, pos.x, pos.y);
-    if (d < 1) {
-      this.total++;
+    if (d < this.total) {
+      this.total = this.total + 1 + h;
+      j++;
+      count++;
       return true;
     } else {
       return false;
     }
   };
+this.munch = function(pos) {
+    let d = dist(this.x, this.y, pos.x, pos.y);
+    if (d < this.total &&count%5===0) {
+     this.total++;
+     h++;
+      j=0;
+      if (j%2===0)
+      {
+        h=0;
+      } 
+      return true;
 
+    } else {
+      return false;
+    }
+  };
   this.dir = function(x, y) {
-    this.xspeed = x;
-    this.yspeed = y;
+    this.xspeed  = x ;
+    this.yspeed  = y ;
+    //this.xspeed  = x * (h%3 +1);
+    //this.yspeed  = y * (h%3 +1);
   };
 
   this.death = function() {
@@ -39,15 +59,17 @@ function Snake() {
   };
 
   this.update = function() {
-    for (let i = 0; i < this.tail.length - 1; i++) {
-      this.tail[i] = this.tail[i + 1];
+    for (let i = 0; i < this.tail.length - 1 - h ; i++) {
+      this.tail[i] = this.tail[i + 1 + h];
     }
-    if (this.total >= 1) {
-      this.tail[this.total - 1] = createVector(this.x, this.y);
+    for (let t = 0; t<=h; t++)
+    {   
+     
+      this.tail[this.total - 1 - t] = createVector(this.x + t*(this.xspeed )  * scl, this.y -t* (this.yspeed )  * scl);
+    
     }
-
-    this.x = this.x + this.xspeed * scl;
-    this.y = this.y + this.yspeed * scl;
+    this.x = this.x + (this.xspeed )  * scl ;
+    this.y = this.y + (this.yspeed )  * scl ;
 
     this.x = constrain(this.x, 0, width - scl);
     this.y = constrain(this.y, 0, height - scl);
@@ -61,3 +83,4 @@ function Snake() {
     rect(this.x, this.y, scl, scl);
   };
 }
+
