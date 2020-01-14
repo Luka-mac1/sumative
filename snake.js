c61950caff54a6237f02f5a0abaa9b30137637e5
Luka-mac1 <55154321+Luka-mac1@users.noreply.github.com>
2020-01-14 13:42:10 -0500

    Update snake.js

--- snake.js
+++ snake.js
@@ -2,22 +2,21 @@
 // http://codingtra.in
 // http://patreon.com/codingtrain
 // Code for: https://youtu.be/AaGK-fj-BAM
-var h=0;
-var j = 0;
-var count = 0 ;
 function Snake() {
-  this.x = 0;
-  this.y = 0;
-  this.xspeed = 1;
+  this.x = 0;   // current head
+  this.y = 0;   // current head
+  this.xspeed = 1; 
   this.yspeed = 0;
   this.total = 0;
   this.tail = [];
+  this.hit = 0;   // counts hits to yum or food
+  this.hityum = 0;   // counts hits to yum
   this.eat = function(pos) {
     let d = dist(this.x, this.y, pos.x, pos.y);
     if (d < 1) {
-      this.total = this.total + 1 + h;
-      j++;
-      count++;
+      this.total++;
+      this.hit++;
+      this.count++;
       return true;
     } else {
       return false;
@@ -25,54 +24,66 @@ function Snake() {
   };
 this.munch = function(pos) {
     let d = dist(this.x, this.y, pos.x, pos.y);
-    if (d < 1 &&count%5===0) {
-     this.total++;
-     h++;
-      j=0;
-      if (j%2===0)
-      {
-        h=0;
-      } 
+    if (d < 1) {
+      this.total++;
+      this.hityum++;  
+      this.total += this.hityum;   
       return true;
-
     } else {
       return false;
     }
   };
-  this.dir = function(x, y) {
+
+this.dir = function(x, y) {
     this.xspeed  = x ;
     this.yspeed  = y ;
-    //this.xspeed  = x * (h%3 +1);
-    //this.yspeed  = y * (h%3 +1);
-  };
+};
 
-  this.death = function() {
+this.death = function() {
     for (let i = 0; i < this.tail.length; i++) {
       let pos = this.tail[i];
       let d = dist(this.x, this.y, pos.x, pos.y);
       if (d < 1) {
         console.log('starting over');
+        this.hit = 0;
+        this.hityum = 0;
         this.total = 0;
         this.tail = [];
       }
     }
   };
 
-  this.update = function() {
-    for (let i = 0; i < this.tail.length - 1 - h ; i++) {
-      this.tail[i] = this.tail[i + 1 + h];
+  this.update = function(fillin) {
+    var tmpx, tmpy;
+
+    // copy old positions including head into the updated tail
+    for (let i = 0; i < this.tail.length - 1 ; i++) {
+      this.tail[i] = this.tail[i + 1];
     }
-    for (let t = 0; t<=h; t++)
-    {   
-     
-      this.tail[this.total - 1 - t]  = createVector(this.x,this.y);
-      this.x = this.x + (this.xspeed )  * scl ;
-      this.y = this.y + (this.yspeed )  * scl ;  
-      this.x = constrain(this.x, 0, width - scl);
+    // always make new head on every move
+    this.tail[this.total - 1]  = createVector(this.x,this.y);      
+    this.x = this.x + (this.xspeed)  * scl ;
+    this.y = this.y + (this.yspeed)  * scl ;  
+    this.x = constrain(this.x, 0, width - scl);
     this.y = constrain(this.y, 0, height - scl);
+
+
+    // if we had a hit extra bits according to new length 
+   /* console.log("total is " + this.total + " length of tail is " + this.tail.length + " and h is " + this.hityum)
+    */
+    if (fillin == true) {
+    for (let t = 0; t<=this.hityum; t++)
+    {  
+      // since we added head to a new possition fill in the holes
+      tmpx = this.x - (this.xspeed + t)  * scl ;
+      tmpy = this.y - (this.yspeed + t)  * scl ;
+
+      this.tail[this.tail.length -  1 - t ] = createVector(tmpx,tmpy);
+      
+   /* console.log("tail at location " + (this.tail.length -  1 - t) + " is " +  this.tail[this.tail.length -  1 - t ]  );
+*/
     }
-    
-    
+  }
   };
 
   this.show = function() {
